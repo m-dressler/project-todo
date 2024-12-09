b90ab0a79543c0bc39a03d99b70d2aa94b542213
Mauritz Dressler <mauritz@prodport.com>
2024-12-09 13:14:08 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project ToDo
+
+Creates a virtual workspace-level `TODO.md` to jot down notes without polluting the filesystem.
+
+## Usage
+
+[Run](https://github.com/orgs/community/discussions/65002#discussioncomment-6828654) command `Project ToDo`.
---
+++ src/extension.ts
@@ -0,0 +1,52 @@
+import * as vscode from "vscode";
+
+const emptyTodo = new TextEncoder().encode(`#${"TODOs"}
+
+ - Add todos here
+`);
+
+const getTodoFileProvider = ({
+  workspaceState,
+}: vscode.ExtensionContext): vscode.FileSystemProvider => {
+  const unsupported = () => {
+    throw new Error("Not supported");
+  };
+
+  return {
+    readFile: ({ path }) => workspaceState.get(path, emptyTodo),
+    writeFile: ({ path }, content) => workspaceState.update(path, content),
+    delete: ({ path }) => workspaceState.update(path, undefined),
+
+    stat: () => ({
+      type: vscode.FileType.File,
+      ctime: Date.now(),
+      mtime: Date.now(),
+      size: 0,
+    }),
+    readDirectory: () => [],
+    createDirectory: unsupported,
+    rename: unsupported,
+    watch: () => new vscode.Disposable(() => {}),
+    onDidChangeFile: new vscode.EventEmitter<vscode.FileChangeEvent[]>().event,
+  };
+};
+
+export function activate(context: vscode.ExtensionContext) {
+  vscode.workspace.registerFileSystemProvider(
+    "project-todo",
+    getTodoFileProvider(context)
+  );
+
+  const subscription = vscode.commands.registerCommand(
+    "project-todo.open",
+    async () => {
+      const virtualFileUri = vscode.Uri.parse("project-todo:/TODOs.md");
+      const doc = await vscode.workspace.openTextDocument(virtualFileUri);
+      await vscode.window.showTextDocument(doc);
+    }
+  );
+
+  context.subscriptions.push(subscription);
+}
+
+export function deactivate() {}
